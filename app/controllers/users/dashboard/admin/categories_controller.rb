03ec74da6133f5ff758df524d7490aa35f7ecb84
Dav1dMwang1 <david.mwangi.john@gmail.com>
2019-11-27 16:46:08 +0300

    CSRF token

--- app/controllers/users/dashboard/admin/categories_controller.rb
+++ app/controllers/users/dashboard/admin/categories_controller.rb
@@ -3,7 +3,6 @@ module Users
     module Admin
       class CategoriesController < AdminDashboardController
         skip_before_action :verify_authenticity_token
-        skip_before_filter :verify_authenticity_token
         protect_from_forgery prepend: true
         before_action :set_category, only: [:show, :edit, :update, :destroy]
 
