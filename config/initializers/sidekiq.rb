e25a7ab3fbfd971e63b137d2b8991a407303488c
Dav1dMwang1 <david.mwangi.john@gmail.com>
2019-11-27 20:01:12 +0300

    Sidekiq config
    Configs sidekiq to auto connect to Redis on a custom port.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 # # Server
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://localhost:7959/0' }
 end
 # # Client
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://localhost:7959/0' }
 end
