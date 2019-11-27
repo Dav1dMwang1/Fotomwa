23c1c066777db1da923c63de4c025283c8f4719a
Dav1dMwang1 <david.mwangi.john@gmail.com>
2019-11-27 15:44:52 +0300

    Skip before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
-
+  skip_before_filter :verify_authenticity_token
   before_action :add_allow_credentials_headers, :cors_preflight_check
-  after_action :cors_set_access_control_headers
-  # skip_before_filter :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :cors_set_access_control_headers
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || users_path
