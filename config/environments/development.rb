3e70a2b93580ee29309ae4a3de10d11d9117c8fe
Dav1dMwang1 <david.mwangi.john@gmail.com>
2019-11-28 13:37:20 +0300

    SMTP port
    Changes the port to 25, for heroku.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
       password:       'jubjmhsjicbshhdd',#ENV['SENDMAIL_PASSWORD'],
       domain:         'smtp.gmail.com',
       address:        'smtp.gmail.com',
-      port:           '587',
+      port:           '25',
       # ssl:            true,
       authentication: :plain,
       enable_starttls_auto: true
