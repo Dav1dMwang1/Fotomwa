b0a8a6b750effecaed7e04149b4336bf3d9d49ba
Dav1dMwang1 <david.mwangi.john@gmail.com>
2019-11-28 14:03:39 +0300

    Net::Config
    Changes the mailer password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       user_name:      'david.mwangi.john@gmail.com',
-      password:       'daIvdBarca2012',
+      password:       'jubjmhsjicbshhdd',
       domain:         'smtp.gmail.com',
       address:        'smtp.gmail.com',
       port:           '587',
